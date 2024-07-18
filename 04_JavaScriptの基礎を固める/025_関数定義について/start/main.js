b37f89e84bdc1347f6b93c948b1f1e6cc4eef3f2
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-07-18 22:01:30 +0900

    関数

--- 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
+++ 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -1,15 +1,31 @@
-const hello = function (name1, age) {
-  // let name1 = 'ken';
-  console.log('hello!' + name1 + age);
-  return name1 + age;
-};
+//なぜ関数を使うのか
+//コンソールに名前を表示したい場合は、文字列をメソッド内に記述する
+console.log('hello name');
 
-// function hello(name1, age) {
-//   // let name1 = 'ken';
-//   console.log('hello!' + name1 + age);
-//   return name1 + age;
-// }
+//この時、名前を動的に変えたいとする
+//変数
+let name1 = 'suzuki';
+console.log('hello ' + name1);
 
-hello('Code Mafia', 10);
-const returnVal = hello('Code Mafia2', 20);
-console.log(returnVal);
+//この時、名前を一つだけでなく沢山の異なる名前を出力したい場合、毎回変数を記述するのは手間になる上にエラーの原因となる可能性がある
+//そこで関数を使用する
+function hello(name1) {
+  // let name1 = 'suzuki'; 変数名を引数に入れる
+  //name1に値を渡すことでコンソールに違う名前を出力することができる
+  console.log('hello ' + name1);
+}
+
+//関数を実行
+hello('tanaka');
+hello('satou');
+hello('ichikawa');
+
+//関数を実行するときに引数()に値を入れると値が変数に格納され、値として使用できるようになる
+
+//引数は複数渡すこともできる
+function hello(name1, age1) {
+  console.log('hello ' + name1 + ', ' + age1);
+}
+hello('tanaka', 21);
+hello('satou', 30);
+hello('ichikawa', 17);
