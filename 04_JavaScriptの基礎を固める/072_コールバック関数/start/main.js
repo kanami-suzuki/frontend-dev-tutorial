3384112fcd27006310c1119aeeb547cdbb64648e
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-08-04 20:51:13 +0900

    コールバック関数

--- 04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
+++ 04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
@@ -18,6 +18,7 @@ hello(getName);
 hello(firstName);
 hello(() => { //無名関数で直接代入することもできる
   return 'name'
+  //helloの引数に関数を渡していることになるため、hello(name)のnameには無名関数が入っている
 });
 //hello()を実行するときにgetName()を渡しているので、引数nameにgetName関数を渡ってきていることになる
 //getName()はhello()の中のname()で実行されている
@@ -30,3 +31,37 @@ hello(() => { //無名関数で直接代入することもできる
 //hello()の中でgetName()と記述すると、getName()を実行するという意味になる
 //getName()は既にhello関数の中でname()として実行されており、実行結果が引数nameに渡っている
 //getName()を実行するとhello()の中で二重に同じ関数が実行されているということになる
+
+//関数内の関数に引数を渡す場合
+function hello2(name, lastName) {
+  console.log(name); //関数自体が渡される
+  console.log(name()); //関数の実行結果が渡される
+  console.log('hello ' + name(lastName));
+}
+//第一引数に無名関数を記述し、第二引数を追加する
+//第二引数に記述した文字列がhello2()の第二引数に渡ってくるので、name()の引数に第二引数を渡す
+//すると、name()を実行した時の第一引数にhello2()の第二引数が格納されるので、hello2()の第一引数である関数の引数に第二引数を入れる
+hello2(function (lastName) {
+  return 'name ' + lastName;
+}, 'tanaka');
+//下記の書き方も同じ
+const getFirstName = function (lastName) {
+  return 'Tom ' + lastName;
+}
+hello2(getFirstName, 'fled');
+
+//同じ関数で計算結果を変える
+function doSumething(a, b, callback) {
+  const result = callback(a, b);
+  console.log(result);
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function plus(a, b) {
+  return a + b;
+}
+doSumething(2, 2, multiply);
+doSumething(2, 3, plus);
