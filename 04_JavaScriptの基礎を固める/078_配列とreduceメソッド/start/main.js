a5e7fa028cebe893fcf8499059781b0295eb51c7
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-08-09 21:48:12 +0900

    reduceメソッド

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,10 @@
-const arry = [1,2,3,4,5];
+//reduceメソッド
+//配列の中身を足し合わせたり、文字列を結合したりするときに使用する
+const arry = [1, 2, 3, 4, 5];
+
+arry.reduce(function (accu, curr) {
+  console.log(accu, curr);
+  return accu + curr;
+});
+//第一引数
+//第二引数
