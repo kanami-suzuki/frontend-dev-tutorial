9c2d9679eb28d9c6b61dc47c49f1faa194393728
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-08-23 22:14:26 +0900

    loadイベント

--- 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
+++ 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
@@ -1,10 +1,17 @@
+//DOMContentLoadedとload
+//DOMContentLoadedとは、DOMツリー(HTML)の文書構造のみを読み込んだ後に発火するイベント
+//loadはDOMツリー(HTML)の文書構造とCSS、画像など全てのコンテンツを読み込んだ後に発火するイベント
 const dcl = document.querySelector('.dcl');
 const load = document.querySelector('.load');
 
+//DOMContentLoadedはdocumentオブジェクトに対してイベントを設定している
 document.addEventListener("DOMContentLoaded", function () {
     dcl.classList.add('done');
 });
 
+//loadはwindowオブジェクトに対してイベントを設定している
 window.addEventListener("load", function () {
     load.classList.add('done');
 });
+//documentオブジェクトはDOM操作ができるもののことを指す。windowオブジェクトはブラウザのwindowを指す
+//DOMContentLoadedはdocumentオブジェクトもwindowオブジェクト登録できるが、loadオブジェクトはwindowオブジェクトのみ登録することができる
