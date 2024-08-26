fea055bfcf3a6e8e0262b0a79a5aafb68404bb18
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-08-26 21:09:50 +0900

    クラス

--- 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
+++ 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
@@ -1,9 +1,15 @@
+//Classという演算子を使用して、文字分割の再利用性を高める
+//animate-titleの他に別な文字列を追加した場合、その度にクラスを増やすのはプログラミング的に助長になってしまう
+//なるべくコード量を減らすために、関数やClass演算子を使用する
+//JavaScriptにおいてのClassとは、オブジェクト指向の概念を実現するための構文。同じプロパティやメソッドを持つオブジェクト(モノ)の設計図となるもの。パーツの制作ができる
+//classの後にキーワードを指定してClassを宣言し、その中でプロパティやメソッドを定義する。クラス内で定義されたメソッドは、そのクラスから作成されたオブジェクトで使用することができる
+//HTMLやCSSのクラスとは全く異なるので注意
 document.addEventListener('DOMContentLoaded', function () {
     const el = document.querySelector('.animate-title');
     const el2 = document.querySelector('.animate-title-2');
     const str = el.innerHTML.trim().split("");
     const str2 = el2.innerHTML.trim().split("");
-    
+
     // let concatStr = '';
 
     // for(let c of str) {
@@ -20,3 +26,8 @@ document.addEventListener('DOMContentLoaded', function () {
         return `${acc}<span class="char">${curr}</span>`;
     }, "");
 });
+
+//クラスを定義(宣言)する。関数のように()は使用しない
+class textAnimation {
+
+}
